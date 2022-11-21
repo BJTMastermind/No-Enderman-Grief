113c8e1b6579b3a9d814c834b372571622735668
BJTMastermind <bjtmastermind@gmail.com>
2022-11-20 19:05:59 -0500

    Updated code for 1.12.2 - Mostly unchanged from 1.7.10

--- src/main/java/com/TheRedMezek/NoEndermanGrief/NoEndermanGrief.java
+++ src/main/java/com/TheRedMezek/NoEndermanGrief/NoEndermanGrief.java
@@ -1,33 +1,33 @@
 package com.TheRedMezek.NoEndermanGrief;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = NoEndermanGrief.MODID, version = NoEndermanGrief.VERSION, acceptedMinecraftVersions = "[1.7.10]")
+@Mod(modid = NoEndermanGrief.MODID, version = NoEndermanGrief.VERSION, acceptedMinecraftVersions = "[1.12.2]")
 public class NoEndermanGrief {
     public static final String MODID = "noendermangrief";
     public static final String VERSION = "1.0";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        EntityEnderman.setCarriable(Blocks.grass, false);
-        EntityEnderman.setCarriable(Blocks.dirt, false);
-        EntityEnderman.setCarriable(Blocks.sand, false);
-        EntityEnderman.setCarriable(Blocks.gravel, false);
-        EntityEnderman.setCarriable(Blocks.yellow_flower, false);
-        EntityEnderman.setCarriable(Blocks.red_flower, false);
-        EntityEnderman.setCarriable(Blocks.brown_mushroom, false);
-        EntityEnderman.setCarriable(Blocks.red_mushroom, false);
-        EntityEnderman.setCarriable(Blocks.tnt, false);
-        EntityEnderman.setCarriable(Blocks.cactus, false);
-        EntityEnderman.setCarriable(Blocks.clay, false);
-        EntityEnderman.setCarriable(Blocks.pumpkin, false);
-        EntityEnderman.setCarriable(Blocks.melon_block, false);
-        EntityEnderman.setCarriable(Blocks.mycelium, false);
+        EntityEnderman.setCarriable(Blocks.GRASS, false);
+        EntityEnderman.setCarriable(Blocks.DIRT, false);
+        EntityEnderman.setCarriable(Blocks.SAND, false);
+        EntityEnderman.setCarriable(Blocks.GRAVEL, false);
+        EntityEnderman.setCarriable(Blocks.YELLOW_FLOWER, false);
+        EntityEnderman.setCarriable(Blocks.RED_FLOWER, false);
+        EntityEnderman.setCarriable(Blocks.BROWN_MUSHROOM, false);
+        EntityEnderman.setCarriable(Blocks.RED_MUSHROOM, false);
+        EntityEnderman.setCarriable(Blocks.TNT, false);
+        EntityEnderman.setCarriable(Blocks.CACTUS, false);
+        EntityEnderman.setCarriable(Blocks.CLAY, false);
+        EntityEnderman.setCarriable(Blocks.PUMPKIN, false);
+        EntityEnderman.setCarriable(Blocks.MELON_BLOCK, false);
+        EntityEnderman.setCarriable(Blocks.MYCELIUM, false);
     }
 
     @EventHandler
